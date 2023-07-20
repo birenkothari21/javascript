54bafcb4b07562814555ba5fc0fefa0ecebd9a13
Biren Kothari <biren.k@prominentpixel.com>
2023-07-20 19:03:34 +0530

    Starting javascript

---
+++ assets/Javascript/app.js
@@ -0,0 +1,5 @@
+let currentResult = 0;
+let calculationDescription = '(currentResult + 10 * 3 / 2 - 1)';
+
+console.log("Answer : ",currentResult + 10 * 3 / 2 - 1);
+console.log(calculationDescription);
