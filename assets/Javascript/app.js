30dd56c7316aa2d88fdb1c762694f152d62e9848
birenkothari21 <139743229+birenkothari21@users.noreply.github.com>
2023-07-21 15:36:17 +0530

    Delete assets directory

--- assets/Javascript/app.js
+++
@@ -1,5 +0,0 @@
-let currentResult = 0;
-let calculationDescription = '(currentResult + 10 * 3 / 2 - 1)';
-
-console.log("Answer : ",currentResult + 10 * 3 / 2 - 1);
-console.log(calculationDescription);
