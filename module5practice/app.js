2016936d51b3b26b460201bf4a8597a83bcfd6eb
Biren Kothari <biren.k@prominentpixel.com>
2023-07-27 18:51:12 +0530

    Add practice files

---
+++ module5practice/app.js
@@ -0,0 +1,40 @@
+/* let name = 'Max';
+
+function greet() {
+    let age = 20;
+    let name = 'Bk';
+    console.log(name, age);
+}
+console.log(name);
+
+greet();
+
+
+console.log(n1);
+var n1 = 10;
+console.log(n1);
+
+console.log(n2);
+let n2 = 10;
+console.log(n2);
+ */
+
+/* function getName() {
+    return prompt("We'll happy if you write your name here : ");
+}
+
+function greet() {
+    let name = getName();
+    console.log(name);
+}
+
+greet();
+ */
+
+// console.log(userName);
+
+'use strict';
+
+userName = 'User';
+
+// console.log(typeof foo);
